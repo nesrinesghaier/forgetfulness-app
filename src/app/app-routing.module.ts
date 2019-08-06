8e292e18624bb6fde12e4e08410d0bcdaa5f72c2
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2019-08-06 15:15:15 +0100

    add important dates and profile front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,13 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},  { path: 'profile', loadChildren: './home/profile/profile.module#ProfilePageModule' },
+  { path: 'dates', loadChildren: './home/dates/dates.module#DatesPageModule' },
+  { path: 'credit', loadChildren: './home/credit/credit.module#CreditPageModule' },
+  { path: 'work', loadChildren: './home/work/work.module#WorkPageModule' },
+  { path: 'measure', loadChildren: './home/measure/measure.module#MeasurePageModule' },
+  { path: 'design', loadChildren: './home/design/design.module#DesignPageModule' },
+
 ];
 
 @NgModule({
