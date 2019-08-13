b72c55629be6213d96ba8a4ffe6556f4e11461a5
Nesrine Sghaier <nesrinesghaier10@gmail.com>
2019-08-13 14:47:31 +0100

    change credits layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,26 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouteReuseStrategy } from '@angular/router';
-import { Ionic4DatepickerModule } from
-      '@logisticinfotech/ionic4-datepicker';
-import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {RouteReuseStrategy} from '@angular/router';
+import {Ionic4DatepickerModule} from '@logisticinfotech/ionic4-datepicker';
+import {IonicModule, IonicRouteStrategy} from '@ionic/angular';
+import {SplashScreen} from '@ionic-native/splash-screen/ngx';
+import {StatusBar} from '@ionic-native/status-bar/ngx';
 
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {NotificationComponent} from './home/dates/notification/notification.component';
+import {CreditModalPageModule} from './home/credit/modal/credit-modal/credit-modal.module';
 
 @NgModule({
-  declarations: [AppComponent],
-  entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), Ionic4DatepickerModule, AppRoutingModule],
-  providers: [
-    StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, NotificationComponent],
+    entryComponents: [NotificationComponent],
+    imports: [BrowserModule, IonicModule.forRoot(), Ionic4DatepickerModule, AppRoutingModule, CreditModalPageModule],
+    providers: [
+        StatusBar,
+        SplashScreen,
+        {provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+    ],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
